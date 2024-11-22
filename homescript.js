ad123017a02c96d3312d3efa34d6ce5aa0db7116
Abdul Wakil Mondal <wakil15032003@gmail.com>
2024-11-23 02:39:16 +0530

    Update Final

--- homescript.js
+++ homescript.js
@@ -1,6 +1,6 @@
 let startBtn = document.getElementById("start-button");
 startBtn.addEventListener("click",()=>{
-    let username = localStorage.getItem("ClipNotesUserName");
+    let username = localStorage.getItem("currentUserName");
     if (username==null)
     {
         window.location.href = "SignUp.html";
